a69566430a085dac9ef70b8a559bb5b18fb557ab
freef <mtfreeland@gmail.com>
2019-02-12 18:19:43 -0500

    add game logic
    add game logic to events.js
    some additional commmits for future features

---
+++ assets/scripts/game/events.js
@@ -0,0 +1,67 @@
+'use strict'
+// lets play tic tac toe
+// generate board
+let gameBoard = {}
+const makeBoard = rowSize => {
+  const newBoard = {
+    board: [],
+    turnCounter: 0,
+    winner: undefined
+  }
+  for (let i = 0; i < rowSize; i++) {
+    if (newBoard.board[i] === undefined) { newBoard.board[i] = [] }
+    for (let j = 0; j < rowSize; j++) {
+      newBoard.board[i].push(' ')
+    }
+  }
+  gameBoard = newBoard
+  return gameBoard
+}
+const placePiece = xy => {
+  if (gameBoard.board[xy[0]][xy[1]] !== ' ') {
+    return 'Not a valid move!'
+  } else {
+    gameBoard.turnCounter % 2 ? gameBoard.board[xy[0]][xy[1]] = 'O' : gameBoard.board[xy[0]][xy[1]] = 'x'
+    gameBoard.turnCounter++
+  }
+}
+const checkWin = (xy) => {
+  const player = gameBoard.turnCounter % 2 ? 'x' : 'O'
+  const winCheck = Boolean(
+    gameBoard.board[xy[0]].every(value => value === player) ||
+    gameBoard.board.every(value => value[xy[1]] === player) ||
+    (gameBoard.board[0][0] === player && gameBoard.board[1][1] === player && gameBoard.board[2][2] === player) ||
+    (gameBoard.board[0][2] === player && gameBoard.board[1][1] === player && gameBoard.board[2][0] === player))
+  if (winCheck === true) { gameBoard.winner = player }
+  return gameBoard
+  // if (gameBoard.board[xy[0]].every(value => value === player)) { // board.board[xy[0]] is an array!
+  //   gameBoard.winner = player
+  // } else if (gameBoard.board.every(value => value[xy[1]] === player)) {
+  //   gameBoard.winner = player
+  // } else if (gameBoard.board[0][0] === player && gameBoard.board[1][1] === player && gameBoard.board[2][2] === player) {
+  //   gameBoard.winner = player
+  // } else if (gameBoard.board[0][2] === player && gameBoard.board[1][1] === player && gameBoard.board[2][0] === player) {
+  //   gameBoard.winner = player
+  // }
+}
+
+const takeTurn = (moveArray) => {
+  if (gameBoard.winner !== undefined) { return `${gameBoard.winner} has already won!` }
+  placePiece(moveArray)
+  if (gameBoard.turnCounter > 4) { checkWin(moveArray) }
+  return gameBoard
+}
+
+console.log(makeBoard(3))
+console.log(takeTurn([1, 1]))
+console.log(takeTurn([1, 1]))
+console.log(takeTurn([0, 0]))
+console.log(takeTurn([0, 2]))
+console.log(takeTurn([1, 0]))
+console.log(takeTurn([2, 0]))
+console.log(takeTurn([2, 2]))
+module.exports = {
+  gameBoard,
+  makeBoard,
+  takeTurn
+}
