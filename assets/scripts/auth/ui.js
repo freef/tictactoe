f15ead73aab10455f910746e9ede0570f8b28c5f
freef <mtfreeland@gmail.com>
2019-02-15 09:53:45 -0500

    Clean UI
    Clean up UI by doing the following:
    - Add X's and O's to game Board
    - Separate log in page from game page
    - Create info bar on top
    - general bugfixes

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,15 +3,23 @@ const store = require('../store.js')
 
 const signUpSuccess = () => {
   $('#user-message').text('Successfully Signed Up!')
+  $('form').trigger('reset')
 }
 const signUpFailure = () => {
   $('#user-message').text('Failed to add user.')
+  $('form').trigger('reset')
 }
 
 const signInSuccess = (responseData) => {
   $('#user-message').text('Successfully Signed In!')
-  $('#current-user').text(`signed in as ${responseData.user.email}`)
+  $('#current-user').text(`${responseData.user.email}`)
+  $('#current-user').removeClass('d-none')
+  $('#play-field').removeClass('d-none')
+  $('#sign-out-form').removeClass('d-none')
+  $('#change-password-content').removeClass('d-none')
+  $('#log-in-content').addClass('d-none')
   store.user = responseData.user
+  $('form').trigger('reset')
 }
 const signInFailure = () => {
   $('#user-message').text('Failure: ')
@@ -21,10 +29,15 @@ const signInFailure = () => {
 const signOutSuccess = () => {
   $('#user-message').text('Successfully Signed Out!')
   $('form').trigger('reset')
+  $('#current-user').addClass('d-none')
+  $('#play-field').addClass('d-none')
+  $('#sign-out-form').addClass('d-none')
+  $('#change-password-content').addClass('d-none')
+  $('form').trigger('reset')
+  $('#log-in-content').removeClass('d-none')
 //  store.user = null
 }
 const signOutFailure = (responseData) => {
-  console.log(responseData.responseText)
   store.response = responseData.responseText
   $('#user-message').text('Failure: ')
   $('form').trigger('reset')
@@ -32,9 +45,11 @@ const signOutFailure = (responseData) => {
 
 const changePasswordSuccess = () => {
   $('#user-message').text('Successfully changed password!')
+  $('form').trigger('reset')
 }
 const changePasswordFailure = () => {
   $('#user-message').text('Failed to change password.')
+  $('form').trigger('reset')
 }
 
 module.exports = {
