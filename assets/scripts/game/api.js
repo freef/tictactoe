a98458feadf4908a908cfb3d0cbfb07208722ed1
freef <mtfreeland@gmail.com>
2019-02-15 10:43:39 -0500

    committing commented out code to allow deployement

--- assets/scripts/game/api.js
+++ assets/scripts/game/api.js
@@ -1,4 +1,33 @@
-'use strict'
-
-module.exports = {
-}
+// 'use strict'
+// // GET
+// // POST
+// // PATCH
+// // DELETE
+//
+// //
+// const changePassword = (formData) => {
+//   //convert to api first then =>
+//   return $.ajax({
+//     url: config.apiUrl + '/change-password',
+//     method: 'PATCH',
+//     headers: {
+//       Authorization: 'Token token=' + store.user.token
+//     },
+//     data: formData
+//   })
+// }
+//
+// // API FORMAT
+// "games": [
+//   {
+//     "id": 1, // CAN BE ADDED
+//     "cells": ["o","x","o","x","o","x","o","x","o"], // MUST BE CONVERTED
+//     "over": true, // CAN BE ADDED
+//     "player_x": { // CAN BE ADDED
+//       "id": 1,
+//       "email": "and@and.com" // CAN BE ADDED
+//     },
+//     "player_o": null // CAN BE ADDED
+//   }
+// module.exports = {
+// }
